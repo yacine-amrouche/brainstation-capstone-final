aba842b35b8e760ab4a1d49cbf1ff103e4071001
yacine <yacineamrouche101091@gmail.com>
2024-02-01 23:11:03 -0500

    adding styling

--- src/Components/FacePage/FacePage.js
+++ src/Components/FacePage/FacePage.js
@@ -1,10 +1,13 @@
+import "./FacePage.scss";
 import React from "react";
 import logo from "../../Assests/images/pexels-scott-webb-28061.jpg";
 
 function FacePage() {
   return (
     <>
-      <img className="home__page" src={logo} alt="home-page"></img>
+      <div className="home">
+        <img className="home__page" src={logo} alt="home-page"></img>
+      </div>
     </>
   );
 }
