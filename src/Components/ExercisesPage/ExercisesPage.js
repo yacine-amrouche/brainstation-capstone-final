cd0897396dbde5e33e4dfd77f46f88dd7c90c1bb
yacine <yacineamrouche101091@gmail.com>
2024-02-02 16:20:54 -0500

    adiing styling

--- src/Components/ExercisesPage/ExercisesPage.js
+++ src/Components/ExercisesPage/ExercisesPage.js
@@ -1,25 +1,28 @@
 import "./ExercisesPage.scss";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ExercisesPage({ exercises }) {
   return (
     <>
       <h1 className="heading__exercises">Shows Results</h1>
-      <p className="heading__info">choose 3 exercises for your workout</p>
+      <h2 className="heading__info">choose 3 exercises for your workout</h2>
       <ul className="heading__sub">
         <li>Beginners: 5-10 minutes each exercise</li>
         <li>Intermediate: 10-20 minutes each exercise</li>
         <li>Advance: 20 minute and above each exercice</li>
       </ul>
 
-      <div className="menu">
+      <Link to="/ExerciceDetail" className="menu">
         {exercises.map((item, i) => (
           <div className="Menu__card" key={`${item}-${i}`}>
-            <img src={item.gifUrl} alt="ex" />
-            <p className="name">{item.bodyPart}</p>
+            <img className="menu_img" src={item.gifUrl} alt="ex" />
+            <button className="btna">{item.bodyPart}</button>
+            <button className="btnb">{item.target}</button>
+            <p className="name">{item.name}</p>
           </div>
         ))}
-      </div>
+      </Link>
     </>
   );
 }
